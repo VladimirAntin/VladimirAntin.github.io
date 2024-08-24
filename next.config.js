e8db1d8dc548a7e4d01d629081ceb493288309ea
Vladimir Antin <antin502@gmail.com>
2024-08-24 19:17:08 +0200

    fix bun

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // eslint-disable-next-line no-undef
-  output: process.env.NODE_ENV === "production" ? 'export' : undefined, // <=== enables static exports
-  reactStrictMode: true,
-  // eslint-disable-next-line no-undef
-  basePath: process.env.NODE_ENV === "production" ? "/" : undefined,
-  // eslint-disable-next-line no-undef
-  assetPrefix: process.env.NODE_ENV === "production" ? "/" : undefined,
 };
 
 export default nextConfig;
