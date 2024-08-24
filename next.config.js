08201c636151612d56d3b447f6fd57d38b251b6f
Vladimir Antin <antin502@gmail.com>
2024-08-24 18:46:03 +0200

    nextjs bun

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export', // <=== enables static exports
   reactStrictMode: true,
+  basePath: 'vladimirantin.github.io',
+  assetPrefix: 'vladimirantin.github.io'
 };
 
 export default nextConfig;
