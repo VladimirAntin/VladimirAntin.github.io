9cdaede8a9593bb3a80f06714f12a2c751929375
Vladimir Antin <antin502@gmail.com>
2024-08-24 18:53:10 +0200

    nextjs bun

--- next.config.js
+++ next.config.js
@@ -3,9 +3,9 @@ const nextConfig = {
   output: 'export', // <=== enables static exports
   reactStrictMode: true,
   // eslint-disable-next-line no-undef
-  basePath: process.env.NODE_ENV === "production" ? "vladimirantin.github.io" : undefined,
+  basePath: process.env.NODE_ENV === "production" ? "/vladimirantin.github.io" : undefined,
   // eslint-disable-next-line no-undef
-  assetPrefix: process.env.NODE_ENV === "production" ? "vladimirantin.github.io" : undefined,
+  assetPrefix: process.env.NODE_ENV === "production" ? "/vladimirantin.github.io" : undefined,
 };
 
 export default nextConfig;
