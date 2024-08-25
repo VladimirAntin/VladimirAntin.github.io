799c9a77ef7c96c8a197d8e9931641a90f9404e5
Vladimir Antin <antin502@gmail.com>
2024-08-25 14:17:22 +0200

    pages cv

--- src/app/cv/page.jsx
+++ src/app/cv/page.jsx
@@ -1,13 +1,16 @@
 import {memo} from 'react';
 import CVViewer from '@/app/components/pdf/CVViewer';
+import DownloadIcon from '@/app/components/icons/DownloadIcon';
 
 const CV = () => {
   return (
     <div className={'flex w-full'}>
-      <div className={'min-h-screen flex flex-col items-center w-1/2 bg-white rounded-2xl grayscale hover:grayscale-0'}>
+      <div className={'min-h-screen flex flex-col items-center w-1/2 bg-white rounded-2xl grayscale hover:grayscale-0  animate-fade-in-left'}>
         <div className={'flex justify-between items-center bg-gray-300 p-4 w-full rounded-t-2xl'}>
           <h1>Vladimir Antin CV</h1>
-          <button>Download</button>
+          <button className={'flex items-center justify-center '}>
+            <DownloadIcon />
+          </button>
         </div>
         <CVViewer />
       </div>
