1e5863efbdaa3db5403fceb30c8483f2d4a66408
Vladimir Antin <antin502@gmail.com>
2026-02-18 21:09:14 +0100

    cname

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // <=== enables static exports
-  basePath: '',
-  assetPrefix: '',
   reactStrictMode: true,
   images: {
     unoptimized: true,
